280ab100b64eed5c75cf69eb1722c61b4a9834f5
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2023-11-16 19:59:20 +0200

    feat: add required processing logic
    Configure calculator to work properly with:
    
     - all number buttons
    
     - "/", "x", "-", "+" operator buttons
    
     - clear and equals buttons

--- script.js
+++ script.js
@@ -1,32 +1,148 @@
-let userOperand1 = 0;
-let userOperator = "";
-let userOperand2 = 0;
+let operand1 = "0";
+let operator = "";
+let operand2 = "";
+let calculationState = "unfinished";
 
-function operate(operand1, operator, operand2) {
+const output = document.querySelector(".output");
+
+function operate() {
   switch (operator) {
     case "+":
-      return add(operand1, operand2);
+      operand1 = add();
+      break;
     case "-":
-      return subtract(operand1, operand2);
+      operand1 = subtract();
+      break;
     case "*":
-      return multiply(operand1, operand2);
+      operand1 = multiply();
+      break;
     case "/":
-      return divide(operand1, operand2);
+      operand1 = divide();
+  }
+
+  output.textContent = operand1;
+}
+
+function add() {
+  return Number(operand1) + Number(operand2);
+}
+
+function subtract() {
+  return Number(operand1) - Number(operand2);
+}
+
+function multiply() {
+  return Number(operand1) * Number(operand2);
+}
+
+function divide() {
+  return Number(operand1) / Number(operand2);
+}
+
+const buttons = document.querySelector(".button-container");
+buttons.addEventListener("click", (event) => {
+  const buttonId = event.target.id;
+  switch (buttonId) {
+    case "clear":
+      handleClear();
+      break;
+    case "+/-":
+      handleChangeSign();
+      break;
+    case "%":
+      handlePercent();
+      break;
+    case ".":
+      handleDot();
+      break;
+    case "=":
+      handleEquals();
+      break;
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      handleOperator(buttonId);
+      break;
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+      handleNumber(buttonId);
+  }
+});
+
+function handleClear() {
+  if (calculationState === "unfinished") {
+    if (operand2 != "") {
+      operand2 = "0";
+      output.textContent = operand2;
+    } else {
+      operand1 = "0";
+      output.textContent = operand1;
+    }
+  } else {
+    operand1 = "0";
+    operator = "";
+    operand2 = "";
+    calculationState = "unfinished";
+    output.textContent = operand1;
   }
 }
 
-function add(operand1, operand2) {
-  return operand1 + operand2;
+function handleChangeSign() {}
+
+function handlePercent() {}
+
+function handleDot() {}
+
+function handleEquals() {
+  if (operand2 === "") {
+    operand2 = operand1;
+  }
+  operate();
+  calculationState = "finished";
 }
 
-function subtract(operand1, operand2) {
-  return operand1 - operand2;
+function handleOperator(op) {
+  if (calculationState === "finished") {
+    operand2 = "";
+    calculationState = "unfinished";
+  } else {
+    if (operand2 != "") {
+      operate();
+      operand2 = "";
+    }
+  }
+  operator = op;
 }
 
-function multiply(operand1, operand2) {
-  return operand1 * operand2;
-}
+function handleNumber(number) {
+  if (operator != "") {
+    if (operand2.length < 7) {
+      if (operand2 === "0") {
+        operand2 = number;
+      } else {
+        operand2 += number;
+      }
 
-function divide(operand1, operand2) {
-  return operand1 / operand2;
+      output.textContent = operand2;
+    }
+  } else {
+    if (operand1.length < 7) {
+      if (operand1 === "0") {
+        operand1 = number;
+      } else {
+        operand1 += number;
+      }
+
+      output.textContent = operand1;
+    }
+  }
 }
